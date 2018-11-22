e5749146029e46c20385ae3ea8adaac6ca0027cc
Imanol <imanolroncomerino@gmail.com>
2018-11-22 09:31:49 +0100

    Sign up works

--- app/src/main/java/activities/signupsigninmobileapp/SignUpActivity.java
+++ app/src/main/java/activities/signupsigninmobileapp/SignUpActivity.java
@@ -1,11 +1,13 @@
 package activities.signupsigninmobileapp;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -29,13 +31,14 @@ import businessLogic.Logic;
 import businessLogic.LogicFactory;
 import exceptions.EmailNotUniqueException;
 import exceptions.LoginExistingException;
+import exceptions.LoginNotExistingException;
+import message.MessageType;
 import model.UserBean;
 
 /**
- *
  * @author Gaizka
  */
-public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener, Thread.UncaughtExceptionHandler {
 
     private static final int PICK_IMAGE = 0;
     ImageButton imgUserPhoto;
@@ -53,6 +56,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
     /**
      * Method to display de view
+     *
      * @param savedInstanceState
      */
     @Override
@@ -80,28 +84,28 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     }
 
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Bitmap oBitmap;
         InputStream oInputStream;
 
-        super.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE){
-            if(data == null){
-                Toast.makeText(this,getResources().getString(R.string.Error), Toast.LENGTH_LONG).show();
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE) {
+            if (data == null) {
+                Toast.makeText(this, getResources().getString(R.string.Error), Toast.LENGTH_LONG).show();
                 return;
             }
             try {
                 oInputStream = this.getApplicationContext().getContentResolver().openInputStream(data.getData());
-                oBitmap = BitmapFactory.decodeStream( oInputStream );
+                oBitmap = BitmapFactory.decodeStream(oInputStream);
                 imgUserPhoto.setImageBitmap(oBitmap);
 
                 ByteArrayOutputStream bos = new ByteArrayOutputStream();
                 oBitmap.compress(Bitmap.CompressFormat.PNG, 100, bos);
                 bArray = bos.toByteArray();
-                my_blob.setBytes(1,bArray);
+                my_blob.setBytes(1, bArray);
             } catch (Exception e) {
-                Toast.makeText( this, getResources().getString(R.string.Exception), Toast.LENGTH_LONG).show();
+                Toast.makeText(this, getResources().getString(R.string.Exception), Toast.LENGTH_LONG).show();
             }
         }
     }
@@ -131,17 +135,17 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         Pattern pattern = Pattern.compile(FULLNAME_PATTERN);
 
         if (fullname == null || fullname.trim().equals("")) {
-            Toast.makeText(this,"Field can not be empty.", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Field can not be empty.", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Field can not be empty.");
         }
 
         if (fullname.trim().length() >= MAX_LENGTH_FULLNAME) {
-            Toast.makeText(this,"Fullname is too long", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Fullname is too long", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Fullname is too long.");
         }
 
         if (!pattern.matcher(fullname).matches()) {
-            Toast.makeText(this,"Full name can only be composed of uppercase and lowercase letters.", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Full name can only be composed of uppercase and lowercase letters.", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Fullname can only be composed of " + "uppercase or lowercase letters.");
         }
     }
@@ -158,20 +162,21 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
 
         if (email == null || email.trim().equals("")) {
-            Toast.makeText(this,"Email is too long", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Email is too long", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Field can not be empty");
         }
 
         if (email.trim().length() >= MAX_LENGTH_EMAIL) {
-            Toast.makeText(this,"Email is too long", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Email is too long", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Email is too long.");
         }
 
         if (!pattern.matcher(email).matches()) {
-            Toast.makeText(this,"Enter a valid email", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Enter a valid email", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Enter a valid email.");
         }
     }
+
     /**
      * Method for validating login.
      *
@@ -183,17 +188,17 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         Pattern pattern = Pattern.compile(LOGIN_PATTERN);
 
         if (login == null || login.trim().equals("")) {
-            Toast.makeText(this,"How are you sopposed to sign in?", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "How are you sopposed to sign in?", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("How are you supposed to sign in?");
         }
 
         if (login.trim().length() >= MAX_LENGTH_LOGIN) {
-            Toast.makeText(this,"Login is too long", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Login is too long", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Login is too long.");
         }
 
         if (!pattern.matcher(login).matches()) {
-            Toast.makeText(this,"Login can only be composed by letters and numbers", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Login can only be composed by letters and numbers", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Login can only be composed by letters and numbers");
         }
     }
@@ -201,7 +206,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     /**
      * Method for validating password.
      *
-     * @param password Password to validate.
+     * @param password        Password to validate.
      * @param confirmPassword Password confirmation.
      * @throws IllegalArgumentException Password is not valid.
      */
@@ -210,27 +215,27 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
 
         if (password == null || password.trim().equals("")) {
-            Toast.makeText(this,"Security first, enter a password.", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Security first, enter a password.", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Security first, enter a password.");
         }
 
         if (password.trim().length() >= MAX_LENGTH_PASSWORD) {
-            Toast.makeText(this,"Password is too long.", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Password is too long.", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Password is too long.");
         }
 
         if (!pattern.matcher(password).matches()) {
-            Toast.makeText(this,"Not secure enough! Try combining lowercase, uppercase and numbers.", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Not secure enough! Try combining lowercase, uppercase and numbers.", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Not secure enough! Try combining lowercase, uppercase and numbers.");
         }
 
         if (password.trim().length() < 6) {
-            Toast.makeText(this,"Password is too short.", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Password is too short.", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Password is too short.");
         }
 
         if (confirmPassword == null || (!confirmPassword.trim().equals(password))) {
-            Toast.makeText(this,"Passwords must coincide.", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Passwords must coincide.", Toast.LENGTH_LONG).show();
             throw new IllegalArgumentException("Passwords must coincide.");
         }
     }
@@ -241,7 +246,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
      */
     private void openSignInWindow() {
         try {
-            Intent intent = new Intent(this,SignInActivity.class);
+            Intent intent = new Intent(this, SignInActivity.class);
             startActivity(intent);
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE,
@@ -253,19 +258,19 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View v) {
-        if(imgUserPhoto.isPressed()){
+        if (imgUserPhoto.isPressed()) {
             Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
             intent.setType("image/*");
             startActivityForResult(intent, PICK_IMAGE);
-        } else if(txtClickHere.isPressed()){
-            Intent intent = new Intent(this,SignInActivity.class);
+        } else if (txtClickHere.isPressed()) {
+            Intent intent = new Intent(this, SignInActivity.class);
             startActivity(intent);
             finish();
-        } else if(hpTermsConditions.isPressed()){
-            Intent browser= new Intent(Intent.ACTION_VIEW, Uri.parse("/PruebaTermsOfUse.pdf"));
+        } else if (hpTermsConditions.isPressed()) {
+            Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse("/PruebaTermsOfUse.pdf"));
             startActivity(browser);
-        }else if(btnSignUp.isPressed()){
-           SignUp();
+        } else if (btnSignUp.isPressed()) {
+            SignUp();
         }
 
     }
@@ -325,29 +330,54 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         */
         // If all fields are filled correctly sign up the user
         if (validFields) {
-            UserBean user = new UserBean();
-            user.setFullName(txtFullName.getText().toString());
-            user.setEmail(txtEmail.getText().toString());
-            user.setLogin(txtLogin.getText().toString());
-            user.setPassword(pwdPassword.getText().toString());
-            //user.setUserPhoto(my_blob);
-
-            Logic logic = new LogicFactory().createImplementation();
-
             try {
-                logic.signUp(user);
+                UserBean user = new UserBean();
+                user.setFullName(txtFullName.getText().toString());
+                user.setEmail(txtEmail.getText().toString());
+                user.setLogin(txtLogin.getText().toString());
+                user.setPassword(pwdPassword.getText().toString());
+                //user.setUserPhoto(my_blob);
+
+                Logic logic = new LogicFactory().createImplementation();
+
+                ConnectionThread connectionThread = new ConnectionThread();
+                connectionThread.setUser(user);
+                connectionThread.setMessage(MessageType.SIGN_UP);
+                connectionThread.setLogic(logic);
+
+                connectionThread.setUncaughtExceptionHandler(this::uncaughtException);
+
+                connectionThread.start();
+                connectionThread.join();
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                builder.setTitle("Info").setMessage("User sign up correctly. Welcome!").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intentSignIn = new Intent(SignUpActivity.this, SignInActivity.class);
+                        startActivity(intentSignIn);
+                        finish();
+                    }
+                });
+                builder.show();
                 // If user is signed up correctly open sign in window
                 openSignInWindow();
 
-            } catch (LoginExistingException e) {
+            } catch (InterruptedException e) {
                 LOGGER.log(Level.SEVERE, "Sign Up controller::handleSignUpAction: {0}", e.getMessage());
-                Toast.makeText(this,"Login already exists.", Toast.LENGTH_LONG).show();
-            } catch (EmailNotUniqueException e) {
-                LOGGER.log(Level.SEVERE, "Sign Up controller::handleSignUpAction: {0}", e.getMessage());
-                Toast.makeText(this,"Email already exists.", Toast.LENGTH_LONG).show();
-            } catch (Exception e) {
-                LOGGER.log(Level.SEVERE, "Sign Up controller::handleSignUpAction: {0}", e.getMessage());
+                Toast.makeText(this, "Connections problems. Please try again.", Toast.LENGTH_LONG).show();
             }
+        }
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        if (e.getCause() instanceof LoginExistingException) {
+            Toast.makeText(this, "Login already exist.", Toast.LENGTH_LONG).show();
+        } else if (e.getCause() instanceof EmailNotUniqueException) {
+            Toast.makeText(this, "Email already used.", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Something went wrong.", Toast.LENGTH_LONG).show();
         }
     }
 }
